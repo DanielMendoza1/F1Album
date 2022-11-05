d0e78205878325d131fd0b604fed67e324dc4bf4
Daniel Alejandro Ornelas <daniel.ornelas192937@potros.itson.edu.mx>
2022-11-05 16:39:28 -0600

    Error Handling for endpoints

--- server/controllers/drivers.controller.js
+++ server/controllers/drivers.controller.js
@@ -1,16 +1,26 @@
 import { pool } from "../db.js";
 
 export const getDrivers = async (req, res) => {
+  try {
     const [result] = await pool.query("SELECT * FROM drivers");
     res.json(result);
-  };
-  
-  export const getDriver = async (req, res) => {
-    const [result] = await pool.query("SELECT * FROM drivers WHERE id_driver = ?", [req.params.id]);
-  
-    if(result.length == 0)
-    return res.status(404).json({message: "Driver not found"});
-  
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const getDriver = async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      "SELECT * FROM drivers WHERE id_driver = ?",
+      [req.params.id]
+    );
+
+    if (result.length == 0)
+      return res.status(404).json({ message: "Driver not found" });
+
     res.json(result[0]);
-  };
-  
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- server/controllers/teams.controllers.js
+++ server/controllers/teams.controllers.js
@@ -1,16 +1,25 @@
 import { pool } from "../db.js";
 
 export const getTeams = async (req, res) => {
-  const [result] = await pool.query("SELECT * FROM teams");
-  res.json(result);
+  try {
+    const [result] = await pool.query("SELECT * FROM teams");
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const getTeam = async (req, res) => {
-  const [result] = await pool.query("SELECT * FROM teams WHERE id_team = ?", [req.params.id]);
+  try {
+    const [result] = await pool.query("SELECT * FROM teams WHERE id_team = ?", [
+      req.params.id,
+    ]);
 
-  if(result.length == 0)
-  return res.status(404).json({message: "Team not found"});
+    if (result.length == 0)
+      return res.status(404).json({ message: "Team not found" });
 
-  res.json(result[0]);
+    res.json(result[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
-
